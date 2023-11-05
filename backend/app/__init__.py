47de68940594e89514696bdd9553cd3a230dd835
BenjaminS <97973081+benjisho@users.noreply.github.com>
2023-11-05 11:54:38 +0200

    added deletion scheduling

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -2,15 +2,21 @@
 import logging
 from flask import Flask
 from flask_cors import CORS
+from apscheduler.schedulers.background import BackgroundScheduler
 
 def create_app():
     app = Flask(__name__)
-    CORS(app, origins=['http://45.32.181.120:3000'])  # Update this line
+    CORS(app, resources={r"/generate-sheet": {"origins": "http://45.32.181.120:3000"}})
 
     # Set up basic logging configuration
     logging.basicConfig(level=logging.DEBUG)
 
     from .routes import main  # Importing routes
     app.register_blueprint(main)
+
+    scheduler = BackgroundScheduler()
+    scheduler.start()
+
+    app.config['SCHEDULER'] = scheduler
 
     return app
--- backend/app/routes.py
+++ backend/app/routes.py
@@ -2,8 +2,19 @@
 from flask import Blueprint, jsonify, current_app
 from googleapiclient.discovery import build
 from google.oauth2 import service_account
+from googleapiclient.errors import HttpError
+from datetime import datetime, timedelta
 
 main = Blueprint('main', __name__)
+
+def delete_sheet(file_id):
+    try:
+        creds = service_account.Credentials.from_service_account_file('app/credentials.json')
+        drive_service = build('drive', 'v3', credentials=creds)
+        drive_service.files().delete(fileId=file_id).execute()
+        current_app.logger.info(f'Deleted file with ID {file_id}')
+    except Exception as e:
+        current_app.logger.error(f'An error occurred: {e}')
 
 @main.route('/generate-sheet', methods=['POST'])
 def generate_sheet():
@@ -19,11 +30,36 @@ def generate_sheet():
         'name': 'Work Attendance Sheet'
     }
     try:
+        # Copy the template file
         result = drive_service.files().copy(
             fileId=template_file_id,
             body=copied_file
         ).execute()
-        return jsonify({'spreadsheetId': result.get('id')}), 200
+        file_id = result.get('id')
+
+        # Set the permissions to 'Anyone with the link'
+        permission = {
+            'type': 'anyone',
+            'role': 'reader',
+        }
+        drive_service.permissions().create(
+            fileId=file_id,
+            body=permission,
+            fields='id',
+        ).execute()
+
+        # Schedule the deletion of the sheet
+        scheduler = current_app.config['SCHEDULER']
+        scheduler.add_job(delete_sheet, 'date', run_date=datetime.utcnow() + timedelta(hours=1), args=[file_id])
+
+        # Construct the URL
+        spreadsheet_url = f'https://docs.google.com/spreadsheets/d/{file_id}'
+
+        return jsonify({'spreadsheetUrl': spreadsheet_url}), 200
+
+    except HttpError as error:
+        current_app.logger.error('An error occurred: %s', error, exc_info=True)
+        return jsonify({'error': 'Failed to generate or share the sheet'}), 500
     except Exception as e:
         current_app.logger.error('An error occurred: %s', e, exc_info=True)
         return jsonify({'error': str(e)}), 500
